45de30bba3d0bc0c98efcb4fb5c8c2b1024ad9e5
Timothy Miller <innerlogic4321@gmail.com>
2023-02-27 19:11:39 +0900

    Start adding tests

---
+++ src/Mauimgur.Tests/ImgurServiceTests.cs
@@ -0,0 +1,32 @@
+// <copyright file="ImgurServiceTests.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using Imgur.API;
+using Imgur.API.Endpoints;
+using Mauimgur.Core.Services;
+
+namespace Mauimgur.Tests;
+
+/// <summary>
+/// Imgur Service Tests.
+/// </summary>
+public class ImgurServiceTests : IClassFixture<ImgurServiceSetup>
+{
+    private ImgurService imgurService;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ImgurServiceTests"/> class.
+    /// </summary>
+    /// <param name="setup">Setup.</param>
+    public ImgurServiceTests(ImgurServiceSetup setup)
+    {
+        this.imgurService = setup.ImgurService;
+    }
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
