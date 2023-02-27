8586e8d64e97ee61a4a5d2ba29861af39f0d9661
Timothy Miller <innerlogic4321@gmail.com>
2023-02-28 00:15:54 +0900

    Update Test

--- src/Mauimgur.Tests/ImgurServiceTests.cs
+++ src/Mauimgur.Tests/ImgurServiceTests.cs
@@ -4,6 +4,7 @@
 
 using Imgur.API;
 using Imgur.API.Endpoints;
+using Mauimgur.Core.Models;
 using Mauimgur.Core.Services;
 
 namespace Mauimgur.Tests;
@@ -27,5 +28,32 @@ public class ImgurServiceTests : IClassFixture<ImgurServiceSetup>
     [Fact]
     public async Task UploadImage()
     {
+        using var token = new CancellationTokenSource();
+        await using var file = File.OpenRead(@"Images/Image.png");
+        var progress = new Progress<ImageUploadUpdate>();
+        progress.ProgressChanged += (sender, update) =>
+        {
+            Assert.NotNull(update.LastUpdated);
+            Assert.NotNull(update.LastUpdated.Link);
+            Assert.True(update.IsDone);
+            token.Cancel();
+        };
+
+        await this.imgurService.UploadImageAsync(file, progress);
+        Assert.True(await this.CompletedTask(10000, token.Token));
+    }
+
+    private async Task<bool> CompletedTask(int timeout, CancellationToken token)
+    {
+        try
+        {
+            await Task.Delay(timeout, token);
+        }
+        catch (TaskCanceledException e)
+        {
+            return true;
+        }
+
+        return token.IsCancellationRequested;
     }
 }
